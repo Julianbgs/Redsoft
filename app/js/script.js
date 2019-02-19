7808cadf01e9b728afdbfada56423d6c8e4bb0ec
Julian <julianbgs96@gmail.com>
2019-02-19 13:04:57 +0300

    finish scripts,mobile menu and menu scroll

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,57 @@
 $(document).ready(function () {
-   // slider-baner
+   //mobile-menu
+
+    $(document).on('click', '#header-trigger', function (event) {
+        event.preventDefault();
+        var menu = $('.header__menu');
+        if ($(this).hasClass('js-trigger-active')) {
+            $(this).removeClass('js-trigger-active');
+            menu.slideUp('slow');
+        } else {
+            $(this).addClass('js-trigger-active');
+            menu.slideDown('slow');
+        }
+        return false;
+    });
+
+
+    //scroll-menu
+    $(document).on('click', '.header__menu li.go-to a', function (event) {
+        event.preventDefault();
+        var selector = $(this);
+        menuScroll(selector);
+    });
+
+    $(document).on('click', '.footer__menu li.go-to a', function (event) {
+        event.preventDefault();
+        var selector = $(this);
+        menuScroll(selector);
+    });
+
+
+    function menuScroll(selector) {
+        if($(selector).hasClass('menu-active')) {
+            $(selector).removeClass('menu-active')
+        } else {
+            $('.header__ li.go-to a').removeClass('menu-active');
+            $(selector).addClass('menu-active');
+        }
+        var href = $(selector).attr('href');
+        var target = $(href);
+        if (window.innerWidth < 710) {
+            var top = target.offset().top;
+            $('html,body').animate({scrollTop: top}, 1000);
+            return false;
+        } else {
+            var top = target.offset().top;
+            $('html,body').animate({scrollTop: top}, 1000);
+            return false;
+        }
+    }
+
+
+
+    // slider-baner
 
     $('.baner__slider').slick({
         infinite: true,
@@ -8,4 +60,6 @@ $(document).ready(function () {
         dots: true,
         arrows: false
     });
+
+
 });
